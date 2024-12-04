6d57f09ecd15ccc916ea9ff3364b2312f7911ce1
Gumas <148417921+Opmasterod@users.noreply.github.com>
2024-12-04 06:56:38 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -52,9 +52,12 @@ async def Cb_Handle(bot: Client, query: CallbackQuery):
         except Exception as e:
             print(e)
 
-    elif data == 'compress':  # Automatically trigger compression at 720p
+    elif data == 'option':  # Automatically trigger compression at 720p
+        file = getattr(query.message.reply_to_message, query.message.reply_to_message.media.value
+        await query.message.edit(text=text, reply_markup=compress)
+
+    elif data.startswith('compress'):
         user_id = query.from_user.id
-        file = getattr(query.message.reply_to_message, query.message.reply_to_message.media.value)
         c_thumb = await db.get_thumbnail(user_id)
         ffmpeg = "-preset veryfast -c:v libx264 -s 1280x720 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 5"
 
